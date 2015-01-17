1a37bf4cbaa331ed1a8038dde00ceb3e9431381d
Felix Jamestin <felixjamestin@gmail.com>
2015-01-17 18:00:30 +0530

    Adding header text animation

--- public_html/templates/default_site/scripts.group/jquery.main.js
+++ public_html/templates/default_site/scripts.group/jquery.main.js
@@ -357,7 +357,7 @@ Header Animation Fuctions
 -------------------------------- */
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 7000, //2500
+	var animationDelay = 5500, //2500
 	//loading bar effect
 	barAnimationDelay = 3800,
 	barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
