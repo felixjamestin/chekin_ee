d442059d5ea4f410408bdf0b51597b2ddc85000b
Felix Jamestin <felixjamestin@gmail.com>
2015-01-17 22:59:51 +0530

    Changing the footer

--- public_html/templates/default_site/scripts.group/jquery.main.js
+++ public_html/templates/default_site/scripts.group/jquery.main.js
@@ -357,7 +357,7 @@ Header Animation Fuctions
 -------------------------------- */
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 3000, //2500
+	var animationDelay = 3500, //2500
 	//loading bar effect
 	barAnimationDelay = 3800,
 	barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
